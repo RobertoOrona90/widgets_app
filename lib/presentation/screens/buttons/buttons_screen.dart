2a686d8fa7e7d265c9d2327bcbc12c5caab4026f
Roberto Orona <jrov.90@gmail.com>
2025-01-10 09:37:27 -0600

    buttons implementation

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   static const String name = 'buttons_screen';
@@ -11,7 +12,48 @@ class ButtonsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Buttons Screen'),
       ),
-      body: Placeholder(),
+      body: ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          context.pop();
+
+        },
+        child: Icon(Icons.arrow_back_ios_new_rounded),
+        ),
+    );
+  }
+}
+
+class ButtonsView extends StatelessWidget {
+  const ButtonsView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Wrap(
+            spacing: 10,
+            crossAxisAlignment: WrapCrossAlignment.center,
+            alignment: WrapAlignment.center,
+            children: [
+                  ElevatedButton(onPressed: (){}, child: const Text('Elevated Button')),
+                  ElevatedButton(onPressed: null, child: const Text('Elevated Button')),
+                  ElevatedButton.icon(onPressed: (){}, icon:Icon(Icons.access_alarm_rounded), label: Text('Elevated Icon')),
+                  FilledButton(onPressed: (){}, child: Text('Filled')),
+                  IconButton(onPressed: (){},
+                   icon: const Icon(Icons.app_registration_outlined),
+                   style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.amber),
+                    iconColor: WidgetStatePropertyAll(Colors.blue)
+                   ),
+                  )
+            ],
+          ),
+      ),
     );
   }
 }
