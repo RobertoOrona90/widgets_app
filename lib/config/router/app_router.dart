03f812e5ffeb9badb54910cfecfac8a6b6140e03
Roberto Orona <jrov.90@gmail.com>
2025-01-10 08:28:21 -0600

    go router by name

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,21 +1,23 @@
 import 'package:go_router/go_router.dart';
 import '../../presentation/screens/screens.dart';
 
-// GoRouter configuration
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(
+      name: HomeScreen.name,
       path: '/',
       builder: (context, state) => HomeScreen(),
     ),
     GoRoute(
+      name: ButtonsScreen.name,
       path: '/buttons',
       builder: (context, state) => ButtonsScreen(),
     ),
     GoRoute(
+      name: CardsScreen.name,
       path: '/cards',
-      builder: (context, state) => HomeScreen(),
+      builder: (context, state) => CardsScreen(),
     ),
   ],
 );
